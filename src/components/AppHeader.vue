36c23968b9ca9f3ef80d2a5cc5b6c37ee3908eed
Saheed Adewale Shittu <saheedadewaleshittu@gmail.com>
2020-06-15 22:00:13 +0100

    add the header and the mini-cart components

---
+++ src/components/AppHeader.vue
@@ -0,0 +1,39 @@
+<template>
+  <nav class="navbar navbar-expand navbar-dark bg-dark">
+    <div class="container">
+    <div class="nav navbar-nav">
+      <a class="nav-item nav-link active" href="#">Home</a>
+      <a class="nav-item nav-link" href="#">Products</a>
+    </div>
+    <div>
+      <div class="btn-group">
+        <button
+          class="btn btn-secondary dropdown-toggle"
+          type="button"
+          id="triggerId"
+          data-toggle="dropdown"
+          aria-haspopup="true"
+          aria-expanded="false"
+        >0 Cart</button>
+        <div>
+          <mini-cart />
+        </div>
+      </div>
+    </div>
+    </div>
+  </nav>
+</template>
+
+<script>
+// eslint-disable-next-line import/extensions
+import MiniCart from './MiniCart';
+
+export default {
+  name: 'AppHeader',
+  // eslint-disable-next-line vue/no-unused-components
+  components: { MiniCart },
+};
+</script>
+
+<style scoped>
+</style>
---
+++ src/components/MiniCart.vue
@@ -0,0 +1,15 @@
+<template>
+    <div class="dropdown-menu dropdown-menu-right" aria-labelledby="triggerId">
+      <a class="dropdown-item" href="#">Action</a>
+      <a class="dropdown-item disabled" href="#">Disabled action</a>
+    </div>
+</template>
+
+<script>
+export default {
+  name: 'MiniCart',
+};
+</script>
+
+<style scoped>
+</style>
